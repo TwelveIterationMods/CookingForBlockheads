abcf1ad91c7bdd76d7a892d63220251fc486a478
Blay09 <blay09@hotmail.de>
2017-01-02 18:51:59 +0100

    Fix cow jar JEI recipe

--- src/main/java/net/blay09/mods/cookingforblockheads/compat/jei/CowJarRecipe.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/compat/jei/CowJarRecipe.java
@@ -3,11 +3,12 @@ package net.blay09.mods.cookingforblockheads.compat.jei;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.blay09.mods.cookingforblockheads.block.ModBlocks;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 public class CowJarRecipe extends BlankRecipeWrapper {
 
-	private ItemStack input = new ItemStack(ModBlocks.milkJar);
+	private ItemStack input = new ItemStack(Blocks.ANVIL);
 	private ItemStack output = new ItemStack(ModBlocks.cowJar);
 
 	@Override
