456eabbde675c845794d5c1a25d4342669a5d425
Blay09 <blay09@hotmail.de>
2017-02-13 18:53:13 +0100

    Revert last commit

--- src/main/java/net/blay09/mods/cookingforblockheads/tile/TileSink.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/tile/TileSink.java
@@ -34,7 +34,7 @@ public class TileSink extends TileEntity {
         @Override
         public int fill(FluidStack resource, boolean doFill) {
             if(!CookingConfig.sinkRequiresWater || resource.getFluid() != FluidRegistry.WATER) {
-                return 0;
+                return resource.amount;
             }
             return super.fill(resource, doFill);
         }
