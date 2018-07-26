50a5e5fe207f88c491474eb06c730a3a2a9b6864
blay09 <blay09@hotmail.de>
2018-07-26 23:05:34 +0200

    Fix glass panes connecting to toaster
    #301

--- src/main/java/net/blay09/mods/cookingforblockheads/block/BlockToaster.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/block/BlockToaster.java
@@ -8,6 +8,7 @@ import net.blay09.mods.cookingforblockheads.tile.TileToaster;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
@@ -122,4 +123,10 @@ public class BlockToaster extends BlockKitchen {
             }
         }
     }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.CENTER;
+    }
+
 }
