5e5bc8b239c022a2bd9ff9ff50826733e8847f24
blay09 <blay09@hotmail.de>
2020-03-16 00:41:07 +0100

    Fix sink rendering

--- src/main/java/net/blay09/mods/cookingforblockheads/client/render/SinkRenderer.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/client/render/SinkRenderer.java
@@ -1,11 +1,17 @@
+
 package net.blay09.mods.cookingforblockheads.client.render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import net.blay09.mods.cookingforblockheads.client.ModModels;
 import net.blay09.mods.cookingforblockheads.tile.SinkTileEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.data.EmptyModelData;
 
 public class SinkRenderer extends TileEntityRenderer<SinkTileEntity> {
 
@@ -15,28 +21,22 @@ public class SinkRenderer extends TileEntityRenderer<SinkTileEntity> {
 
     @Override
     public void render(SinkTileEntity tileEntity, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
-        if (!tileEntity.hasWorld()) {
+        World world = tileEntity.getWorld();
+        if (world == null) {
             return;
         }
 
-        RenderHelper.disableStandardItemLighting();
-        // TODO GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        // TODO GlStateManager.enableBlend();
-        // TODO GlStateManager.disableCull();
-
-        if (tileEntity.getWaterAmount() > 0) {
-            // TODO GlStateManager.enableBlend();
+        BlockRendererDispatcher dispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
+        int waterAmount = tileEntity.getWaterAmount();
+        int capacity = tileEntity.getWaterCapacity();
+        if (waterAmount > 0) {
             matrixStack.push();
-            matrixStack.translate(0, 0.5f, 0);
-            float filledPercentage = tileEntity.getWaterAmount() / (float) tileEntity.getWaterCapacity();
+            float filledPercentage = waterAmount / (float) capacity;
+            matrixStack.translate(0f, 0.5f - 0.5f * filledPercentage, 0f);
             matrixStack.scale(1f, filledPercentage, 1f);
-            matrixStack.translate(0f, -0.51f, 0f);
-            // TODO bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
-            // TODO Minecraft.getInstance().getItemRenderer().renderModel(ModModels.sinkLiquid, Fluids.WATER.getAttributes().getColor());
+            dispatcher.getBlockModelRenderer().renderModel(world, ModModels.sinkLiquid, tileEntity.getBlockState(), tileEntity.getPos(), matrixStack, buffer.getBuffer(RenderType.solid()), false, world.rand, 0, 0, EmptyModelData.INSTANCE);
             matrixStack.pop();
         }
-
-        RenderHelper.enableStandardItemLighting();
     }
 
 }
