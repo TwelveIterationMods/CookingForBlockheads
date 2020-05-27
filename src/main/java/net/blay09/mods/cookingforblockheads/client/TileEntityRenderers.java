1827e9635ac9707969d3c31b0543604f781c909b
blay09 <blay09@hotmail.de>
2020-05-27 17:37:18 +0200

    Add crash if blocks aren't initialized at ColorHandlerEvent, since it means that something else crashed earlier and hopefully this will make people not report it to me anymore
    #444

--- src/main/java/net/blay09/mods/cookingforblockheads/client/TileEntityRenderers.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/client/TileEntityRenderers.java
@@ -1,16 +1,18 @@
 package net.blay09.mods.cookingforblockheads.client;
 
+
 import net.blay09.mods.cookingforblockheads.CookingForBlockheads;
 import net.blay09.mods.cookingforblockheads.block.BlockKitchen;
 import net.blay09.mods.cookingforblockheads.block.ModBlocks;
 import net.blay09.mods.cookingforblockheads.client.render.*;
-import net.blay09.mods.cookingforblockheads.tile.*;
+import net.blay09.mods.cookingforblockheads.tile.ModTileEntities;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
 
 @Mod.EventBusSubscriber(modid = CookingForBlockheads.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TileEntityRenderers {
@@ -33,6 +35,10 @@ public class TileEntityRenderers {
 
     @SubscribeEvent
     public static void initBlockColors(ColorHandlerEvent.Block event) {
+        if (ModBlocks.fridge == null) {
+            throw new RuntimeException("Something crashed the event bus earlier in the loading stage. This is NOT a Cooking for Blockheads issue! Scroll up in your latest.log to find the real crash log.");
+        }
+
         event.getBlockColors().register((state, world, pos, i) -> state.get(BlockKitchen.COLOR).getColorValue(), ModBlocks.fridge);
         event.getBlockColors().register((state, world, pos, i) -> 4159204, ModBlocks.sink);
     }
