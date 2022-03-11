bdf1bbcb09a37f86396c2bc6aee1301253095bb6
blay09 <blay09@hotmail.de>
2022-03-11 10:51:16 +0100

    Fix some startup crashes

--- shared/src/main/java/net/blay09/mods/cookingforblockheads/tile/ModBlockEntities.java
+++ shared/src/main/java/net/blay09/mods/cookingforblockheads/tile/ModBlockEntities.java
@@ -5,6 +5,7 @@ import net.blay09.mods.balm.api.block.BalmBlockEntities;
 import net.blay09.mods.cookingforblockheads.CookingForBlockheads;
 import net.blay09.mods.cookingforblockheads.block.ModBlocks;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class ModBlockEntities {
@@ -25,20 +26,20 @@ public class ModBlockEntities {
     public static DeferredObject<BlockEntityType<CuttingBoardBlockEntity>> cuttingBoard;
 
     public static void initialize(BalmBlockEntities blockEntities) {
-        cookingTable = blockEntities.registerBlockEntity(id("cooking_table"), CookingTableBlockEntity::new, () -> ModBlocks.cookingTable);
-        oven = blockEntities.registerBlockEntity(id("oven"), OvenBlockEntity::new, () -> ModBlocks.oven);
-        fridge = blockEntities.registerBlockEntity(id("fridge"), FridgeBlockEntity::new, () -> ModBlocks.fridge);
-        sink = blockEntities.registerBlockEntity(id("sink"), SinkBlockEntity::new, () -> ModBlocks.sink);
-        toolRack = blockEntities.registerBlockEntity(id("tool_rack"), ToolRackBlockEntity::new, () -> ModBlocks.toolRack);
-        toaster = blockEntities.registerBlockEntity(id("toaster"), ToasterBlockEntity::new, () -> ModBlocks.toaster);
-        milkJar = blockEntities.registerBlockEntity(id("milk_jar"), MilkJarBlockEntity::new, () -> ModBlocks.milkJar);
-        cowJar = blockEntities.registerBlockEntity(id("cow_jar"), CowJarBlockEntity::new, () -> ModBlocks.cowJar);
-        spiceRack = blockEntities.registerBlockEntity(id("spice_rack"), SpiceRackBlockEntity::new, () -> ModBlocks.spiceRack);
-        counter = blockEntities.registerBlockEntity(id("counter"), CounterBlockEntity::new, () -> ModBlocks.counter);
-        cabinet = blockEntities.registerBlockEntity(id("cabinet"), CabinetBlockEntity::new, () -> ModBlocks.cabinet);
-        corner = blockEntities.registerBlockEntity(id("corner"), CornerBlockEntity::new, () -> ModBlocks.corner);
-        fruitBasket = blockEntities.registerBlockEntity(id("fruit_basket"), FruitBasketBlockEntity::new, () -> ModBlocks.fruitBasket);
-        cuttingBoard = blockEntities.registerBlockEntity(id("cutting_board"), CuttingBoardBlockEntity::new, () -> ModBlocks.cuttingBoard);
+        cookingTable = blockEntities.registerBlockEntity(id("cooking_table"), CookingTableBlockEntity::new, () -> new Block[] {ModBlocks.cookingTable});
+        oven = blockEntities.registerBlockEntity(id("oven"), OvenBlockEntity::new, () -> new Block[] {ModBlocks.oven});
+        fridge = blockEntities.registerBlockEntity(id("fridge"), FridgeBlockEntity::new, () -> new Block[] {ModBlocks.fridge});
+        sink = blockEntities.registerBlockEntity(id("sink"), SinkBlockEntity::new, () -> new Block[] {ModBlocks.sink});
+        toolRack = blockEntities.registerBlockEntity(id("tool_rack"), ToolRackBlockEntity::new, () -> new Block[] {ModBlocks.toolRack});
+        toaster = blockEntities.registerBlockEntity(id("toaster"), ToasterBlockEntity::new, () -> new Block[] {ModBlocks.toaster});
+        milkJar = blockEntities.registerBlockEntity(id("milk_jar"), MilkJarBlockEntity::new, () -> new Block[] {ModBlocks.milkJar});
+        cowJar = blockEntities.registerBlockEntity(id("cow_jar"), CowJarBlockEntity::new, () -> new Block[] {ModBlocks.cowJar});
+        spiceRack = blockEntities.registerBlockEntity(id("spice_rack"), SpiceRackBlockEntity::new, () -> new Block[] {ModBlocks.spiceRack});
+        counter = blockEntities.registerBlockEntity(id("counter"), CounterBlockEntity::new, () ->new Block[] {ModBlocks.counter});
+        cabinet = blockEntities.registerBlockEntity(id("cabinet"), CabinetBlockEntity::new, () ->new Block[] {ModBlocks.cabinet});
+        corner = blockEntities.registerBlockEntity(id("corner"), CornerBlockEntity::new, () -> new Block[] {ModBlocks.corner});
+        fruitBasket = blockEntities.registerBlockEntity(id("fruit_basket"), FruitBasketBlockEntity::new, () -> new Block[] {ModBlocks.fruitBasket});
+        cuttingBoard = blockEntities.registerBlockEntity(id("cutting_board"), CuttingBoardBlockEntity::new, () -> new Block[] {ModBlocks.cuttingBoard});
     }
 
     private static ResourceLocation id(String name) {
