e166a07cf0c60cb4de9927fac669e89cf0c10b18
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2023-06-11 17:03:00 +0200

    fix: Fix compile error in mod menu integration

--- fabric/src/main/java/net/blay09/mods/cookingforblockheads/compat/ModMenuIntegration.java
+++ fabric/src/main/java/net/blay09/mods/cookingforblockheads/compat/ModMenuIntegration.java
@@ -2,7 +2,7 @@ package net.blay09.mods.cookingforblockheads.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.blay09.mods.balm.fabric.config.FabricBalmConfig;
+import net.blay09.mods.balm.fabric.compat.ModMenuUtils;
 import net.blay09.mods.cookingforblockheads.CookingForBlockheadsConfigData;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -11,6 +11,6 @@ import net.fabricmc.api.Environment;
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return FabricBalmConfig.getConfigScreen(CookingForBlockheadsConfigData.class);
+        return ModMenuUtils.getConfigScreen(CookingForBlockheadsConfigData.class);
     }
 }
