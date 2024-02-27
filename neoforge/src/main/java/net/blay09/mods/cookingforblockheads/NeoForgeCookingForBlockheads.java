1f41b8a88c6689ed13c31e65ad3491afc06f7f6b
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2024-02-27 08:52:41 +0100

    fix: Fix error when opening cooking table on NeoForge

--- neoforge/src/main/java/net/blay09/mods/cookingforblockheads/NeoForgeCookingForBlockheads.java
+++ neoforge/src/main/java/net/blay09/mods/cookingforblockheads/NeoForgeCookingForBlockheads.java
@@ -2,20 +2,33 @@ package net.blay09.mods.cookingforblockheads;
 
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.neoforge.provider.NeoForgeBalmProviders;
+import net.blay09.mods.cookingforblockheads.api.KitchenItemProcessor;
+import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.api.event.OvenItemSmeltedEvent;
+import net.blay09.mods.cookingforblockheads.block.entity.ModBlockEntities;
 import net.blay09.mods.cookingforblockheads.client.CookingForBlockheadsClient;
 import net.blay09.mods.cookingforblockheads.compat.Compat;
 import net.blay09.mods.cookingforblockheads.compat.TheOneProbeAddon;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
+import net.neoforged.neoforge.capabilities.BlockCapability;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 @Mod(CookingForBlockheads.MOD_ID)
 public class NeoForgeCookingForBlockheads {
+
+    private static final BlockCapability<KitchenItemProvider, Void> KITCHEN_ITEM_PROVIDER = BlockCapability.createVoid(new ResourceLocation(CookingForBlockheads.MOD_ID,
+            "kitchen_item_provider"), KitchenItemProvider.class);
+    private static final BlockCapability<KitchenItemProcessor, Void> KITCHEN_ITEM_PROCESSOR = BlockCapability.createVoid(new ResourceLocation(
+            CookingForBlockheads.MOD_ID,
+            "kitchen_item_processor"), KitchenItemProcessor.class);
 
     public NeoForgeCookingForBlockheads(IEventBus eventBus) {
         Balm.getEvents().onEvent(OvenItemSmeltedEvent.class, orig -> {
@@ -26,12 +39,27 @@ public class NeoForgeCookingForBlockheads {
         Balm.initialize(CookingForBlockheads.MOD_ID, CookingForBlockheads::initialize);
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(CookingForBlockheads.MOD_ID, CookingForBlockheadsClient::initialize));
 
+        eventBus.addListener(this::registerCapabilities);
         eventBus.addListener(this::enqueueIMC);
+
+        final var providers = ((NeoForgeBalmProviders) Balm.getProviders());
+        providers.registerBlockProvider(KitchenItemProvider.class, KITCHEN_ITEM_PROVIDER);
+        providers.registerBlockProvider(KitchenItemProcessor.class, KITCHEN_ITEM_PROCESSOR);
     }
 
     private void enqueueIMC(InterModEnqueueEvent event) {
         if (Balm.isModLoaded(Compat.THEONEPROBE)) {
             TheOneProbeAddon.register();
         }
+    }
+
+    private void registerCapabilities(RegisterCapabilitiesEvent event) {
+        event.registerBlockEntity(KITCHEN_ITEM_PROVIDER, ModBlockEntities.milkJar.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProvider.class));
+        event.registerBlockEntity(KITCHEN_ITEM_PROVIDER, ModBlockEntities.cowJar.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProvider.class));
+        event.registerBlockEntity(KITCHEN_ITEM_PROVIDER, ModBlockEntities.fridge.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProvider.class));
+        event.registerBlockEntity(KITCHEN_ITEM_PROVIDER, ModBlockEntities.sink.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProvider.class));
+        event.registerBlockEntity(KITCHEN_ITEM_PROVIDER, ModBlockEntities.cuttingBoard.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProvider.class));
+
+        event.registerBlockEntity(KITCHEN_ITEM_PROCESSOR, ModBlockEntities.oven.get(), (blockEntity, context) -> blockEntity.getProvider(KitchenItemProcessor.class));
     }
 }
