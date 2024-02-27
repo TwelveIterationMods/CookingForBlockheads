3a90bde96dbc71573d81520ca2206545e4dc4e58
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2024-02-27 08:33:47 +0100

    fix: Fix item provider tag not being respected on Fabric

--- fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/FabricCookingForBlockheads.java
+++ fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/FabricCookingForBlockheads.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.cookingforblockheads.fabric;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.container.BalmContainerProvider;
 import net.blay09.mods.balm.api.fluid.FluidTank;
 import net.blay09.mods.balm.common.BalmBlockEntity;
 import net.blay09.mods.balm.fabric.provider.FabricBalmProviders;
@@ -8,9 +9,12 @@ import net.blay09.mods.cookingforblockheads.CookingForBlockheads;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProcessor;
 import net.blay09.mods.cookingforblockheads.block.entity.ModBlockEntities;
+import net.blay09.mods.cookingforblockheads.kitchen.ContainerKitchenItemProvider;
+import net.blay09.mods.cookingforblockheads.tag.ModBlockTags;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class FabricCookingForBlockheads implements ModInitializer {
@@ -26,6 +30,20 @@ public class FabricCookingForBlockheads implements ModInitializer {
 
         registerLookup(new ResourceLocation("balm", "fluid_tank"), FluidTank.class,
                 ModBlockEntities.sink.get(), ModBlockEntities.milkJar.get(), ModBlockEntities.cowJar.get());
+
+        var itemProviderLookup = BlockApiLookup.get(new ResourceLocation(CookingForBlockheads.MOD_ID, "kitchen_item_provider"),
+                KitchenItemProvider.class,
+                Void.class);
+        itemProviderLookup.registerFallback((level, pos, state, blockEntity, context) -> {
+            if (state.is(ModBlockTags.KITCHEN_ITEM_PROVIDERS)) {
+                if (blockEntity instanceof Container container) {
+                    return new ContainerKitchenItemProvider(container);
+                } else if(blockEntity instanceof BalmContainerProvider containerProvider) {
+                    return new ContainerKitchenItemProvider(containerProvider.getContainer());
+                }
+            }
+            return null;
+        });
     }
 
     private <T> void registerProvider(String name, Class<T> clazz, BlockEntityType<?>... blockEntities) {
