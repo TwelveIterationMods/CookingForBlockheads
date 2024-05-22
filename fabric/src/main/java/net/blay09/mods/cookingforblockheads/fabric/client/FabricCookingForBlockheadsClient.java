de609335e6da524156c764a2ed4d650a632cec3a
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2024-05-22 20:29:32 +0200

    fix: Fix compile error again

--- fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/client/FabricCookingForBlockheadsClient.java
+++ fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/client/FabricCookingForBlockheadsClient.java
@@ -1,5 +1,6 @@
 package net.blay09.mods.cookingforblockheads.fabric.client;
 
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.cookingforblockheads.CookingForBlockheads;
 import net.blay09.mods.cookingforblockheads.client.CookingForBlockheadsClient;
@@ -9,6 +10,6 @@ public class FabricCookingForBlockheadsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BalmClient.initialize(CookingForBlockheads.MOD_ID, CookingForBlockheadsClient::initialize);
+        BalmClient.initialize(CookingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, CookingForBlockheadsClient::initialize);
     }
 }
