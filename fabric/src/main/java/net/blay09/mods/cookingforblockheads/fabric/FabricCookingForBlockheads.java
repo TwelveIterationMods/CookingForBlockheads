fe57669e4646a460862b365a2e4a9deab4759160
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2025-03-25 17:03:55 +0100

    feat: Update to Minecraft 1.21.5

--- fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/FabricCookingForBlockheads.java
+++ fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/FabricCookingForBlockheads.java
@@ -3,42 +3,19 @@ package net.blay09.mods.cookingforblockheads.fabric;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.container.BalmContainerProvider;
-import net.blay09.mods.balm.api.fluid.FluidTank;
-import net.blay09.mods.balm.common.BalmBlockEntity;
-import net.blay09.mods.balm.fabric.provider.FabricBalmProviders;
 import net.blay09.mods.cookingforblockheads.CookingForBlockheads;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
-import net.blay09.mods.cookingforblockheads.api.KitchenItemProcessor;
-import net.blay09.mods.cookingforblockheads.block.entity.ModBlockEntities;
 import net.blay09.mods.cookingforblockheads.kitchen.ContainerKitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.tag.ModBlockTags;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class FabricCookingForBlockheads implements ModInitializer {
     @Override
     public void onInitialize() {
         Balm.initialize(CookingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, CookingForBlockheads::initialize);
-
-        registerProvider("kitchen_item_provider",
-                KitchenItemProvider.class,
-                ModBlockEntities.milkJar.get(),
-                ModBlockEntities.cowJar.get(),
-                ModBlockEntities.fridge.get(),
-                ModBlockEntities.sink.get(),
-                ModBlockEntities.cuttingBoard.get(),
-                ModBlockEntities.counter.get(),
-                ModBlockEntities.cabinet.get(),
-                ModBlockEntities.fruitBasket.get(),
-                ModBlockEntities.spiceRack.get());
-
-        registerProvider("kitchen_item_processor", KitchenItemProcessor.class, ModBlockEntities.oven.get());
-
-        registerLookup(ResourceLocation.fromNamespaceAndPath("balm", "fluid_tank"), FluidTank.class,
-                ModBlockEntities.sink.get(), ModBlockEntities.milkJar.get(), ModBlockEntities.cowJar.get());
 
         var itemProviderLookup = BlockApiLookup.get(ResourceLocation.fromNamespaceAndPath(CookingForBlockheads.MOD_ID, "kitchen_item_provider"),
                 KitchenItemProvider.class,
@@ -55,15 +32,4 @@ public class FabricCookingForBlockheads implements ModInitializer {
         });
     }
 
-    private <T> void registerProvider(String name, Class<T> clazz, BlockEntityType<?>... blockEntities) {
-        var providers = ((FabricBalmProviders) Balm.getProviders());
-        ResourceLocation identifier = ResourceLocation.fromNamespaceAndPath(CookingForBlockheads.MOD_ID, name);
-        providers.registerProvider(identifier, clazz);
-        registerLookup(identifier, clazz, blockEntities);
-    }
-
-    private <T> void registerLookup(ResourceLocation identifier, Class<T> clazz, BlockEntityType<?>... blockEntities) {
-        var lookup = BlockApiLookup.get(identifier, clazz, Void.class);
-        lookup.registerForBlockEntities((blockEntity, context) -> ((BalmBlockEntity) blockEntity).getProvider(clazz), blockEntities);
-    }
 }
