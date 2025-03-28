9dd9524776bfc93a133bc56eb0b0e0ee710d5c3f
BlayTheNinth <1933180+blay09@users.noreply.github.com>
2025-03-28 15:07:58 +0100

    fix(1.21.5): Update capabilities on Forge

--- forge/src/main/java/net/blay09/mods/cookingforblockheads/ForgeCookingForBlockheads.java
+++ forge/src/main/java/net/blay09/mods/cookingforblockheads/ForgeCookingForBlockheads.java
@@ -3,29 +3,26 @@ package net.blay09.mods.cookingforblockheads;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.balm.forge.ForgeLoadContext;
-import net.blay09.mods.balm.forge.provider.ForgeBalmProviders;
-import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
+import net.blay09.mods.balm.forge.capability.ForgeBalmCapabilities;
 import net.blay09.mods.cookingforblockheads.api.KitchenItemProcessor;
+import net.blay09.mods.cookingforblockheads.api.KitchenItemProvider;
 import net.blay09.mods.cookingforblockheads.api.event.OvenItemSmeltedEvent;
 import net.blay09.mods.cookingforblockheads.client.CookingForBlockheadsClient;
 import net.blay09.mods.cookingforblockheads.compat.Compat;
 import net.blay09.mods.cookingforblockheads.compat.TheOneProbeAddon;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.*;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 
+import static net.blay09.mods.cookingforblockheads.CookingForBlockheads.id;
+
 @Mod(CookingForBlockheads.MOD_ID)
 public class ForgeCookingForBlockheads {
-
-    public static Capability<KitchenItemProvider> KITCHEN_ITEM_PROVIDER_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
-    });
-
-    public static Capability<KitchenItemProcessor> KITCHEN_ITEM_PROCESSOR_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {
-    });
 
     public ForgeCookingForBlockheads(FMLJavaModLoadingContext context) {
         final var loadContext = new ForgeLoadContext(context.getModEventBus());
@@ -34,19 +31,18 @@ public class ForgeCookingForBlockheads {
             MinecraftForge.EVENT_BUS.post(event);
         });
 
-        Balm.initialize(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheads::initialize);
+        final var forgeCapabilities = (ForgeBalmCapabilities) Balm.getCapabilities();
+        forgeCapabilities.preRegisterType(id("kitchen_item_provider"), CapabilityManager.get(new CapabilityToken<KitchenItemProvider>() {
+        }));
+        forgeCapabilities.preRegisterType(id("kitchen_item_processor"), CapabilityManager.get(new CapabilityToken<KitchenItemProcessor>() {
+        }));
+
+        Balm.initializeMod(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheads::initialize);
         if (FMLEnvironment.dist.isClient()) {
-            BalmClient.initialize(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheadsClient::initialize);
+            BalmClient.initializeMod(CookingForBlockheads.MOD_ID, loadContext, CookingForBlockheadsClient::initialize);
         }
 
-        context.getModEventBus().addListener(this::registerCapabilities);
         context.getModEventBus().addListener(this::enqueueIMC);
-
-        ForgeBalmProviders providers = (ForgeBalmProviders) Balm.getProviders();
-        providers.register(KitchenItemProvider.class, new CapabilityToken<>() {
-        });
-        providers.register(KitchenItemProcessor.class, new CapabilityToken<>() {
-        });
     }
 
     private void enqueueIMC(InterModEnqueueEvent event) {
@@ -55,8 +51,4 @@ public class ForgeCookingForBlockheads {
         }
     }
 
-    private void registerCapabilities(RegisterCapabilitiesEvent event) {
-        event.register(KitchenItemProvider.class);
-        event.register(KitchenItemProcessor.class);
-    }
 }
