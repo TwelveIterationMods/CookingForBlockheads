16489c173d48e456100121eaa4d8f4b9536e4ed8
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-21 22:43:05 +0200

    fix: Fix config i18n keys

--- common/src/main/java/net/blay09/mods/cookingforblockheads/CookingForBlockheadsConfig.java
+++ common/src/main/java/net/blay09/mods/cookingforblockheads/CookingForBlockheadsConfig.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.cookingforblockheads;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.common.config.ConfigLocalization;
 
 public class CookingForBlockheadsConfig {
     public static CookingForBlockheadsConfigData getActive() {
@@ -8,6 +9,7 @@ public class CookingForBlockheadsConfig {
     }
 
     public static void initialize() {
+        ConfigLocalization.enableModernTranslationKeys(CookingForBlockheads.MOD_ID);
         Balm.getConfig().registerConfig(CookingForBlockheadsConfigData.class, null);
     }
 }
