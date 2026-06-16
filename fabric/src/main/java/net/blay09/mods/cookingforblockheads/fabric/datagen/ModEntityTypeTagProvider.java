e360afd501e213f9b0ddb765a827ba22a1a12272
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-16 19:29:17 +0200

    refactor(26.2): ModEntityTypeTagProvider -> FabricTagsProvider

--- fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/datagen/ModEntityTypeTagProvider.java
+++ fabric/src/main/java/net/blay09/mods/cookingforblockheads/fabric/datagen/ModEntityTypeTagProvider.java
@@ -2,15 +2,17 @@ package net.blay09.mods.cookingforblockheads.fabric.datagen;
 
 import net.blay09.mods.cookingforblockheads.tag.ModEntityTypeTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityTypes;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModEntityTypeTagProvider extends EntityTypeTagsProvider {
+public class ModEntityTypeTagProvider extends FabricTagsProvider<EntityType<?>> {
     public ModEntityTypeTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, registriesFuture);
+        super(output, Registries.ENTITY_TYPE, registriesFuture);
     }
 
     @Override
